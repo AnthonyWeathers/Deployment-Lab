17e701673f0fed50e6371b9f9bb1bcfa6975ec7c
Anthony Weathers <anthonyweathers115@gmail.com>
2024-02-28 13:45:03 -0800

    Added another video, background image, and styling

--- public/main.js
+++ public/main.js
@@ -1,9 +1,11 @@
 const showAni = document.getElementById('showAnimation');
 const video = document.getElementById('video');
+const showSong = document.getElementById('showSong');
+const song = document.getElementById('song');
 
 
 const showAniHandler = e => {
-    video.classList.toggle('hidden')
+    video.classList.toggle('hiddenVideo')
     if(showAni.textContent === 'Show Video') {
         //video.classList.remove('hidden')
         showAni.textContent = 'Hide Video'
@@ -13,4 +15,16 @@ const showAniHandler = e => {
     }
 }
 
+const showSongHandler = e => {
+    song.classList.toggle('hiddenSong')
+    if(showSong.textContent === 'Show Song') {
+        //video.classList.remove('hidden')
+        showSong.textContent = 'Hide Song'
+    } else {
+        //video.classList.add('hidden')
+        showSong.textContent = 'Show Song'
+    }
+}
+
 showAni.addEventListener('click', showAniHandler);
+showSong.addEventListener('click', showSongHandler);
